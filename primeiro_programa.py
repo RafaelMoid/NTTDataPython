aa1ee2ded294e62d06efab23e4600f6ad213cca6
rafael <rafaelvrcr@gmail.com>
2024-09-01 13:24:40 -0300

    Tipos de dados

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,11 @@
 print("Bora nessa parceiro")
+
+# Aula 2 - Tipos de dados
+print(17)
+print(11 + 32)
+print(25 * 2)
+print((11 + 4) * 2)
+print(17 / 2)
+print(True)
+print(False)
+print("Concatenação " + "de " + "strings.")
