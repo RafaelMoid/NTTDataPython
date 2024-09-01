f080538a86f3549a31f0b03671191768c6cb9476
rafael <rafaelvrcr@gmail.com>
2024-09-01 14:34:38 -0300

    Declarando variaveis

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,6 +1,7 @@
 print("Bora nessa parceiro")
 
 # Aula 2 - Tipos de dados
+print('Aula sobre Tipos de dados')
 print(17)
 print(11 + 32)
 print(25 * 2)
@@ -9,3 +10,20 @@ print(17 / 2)
 print(True)
 print(False)
 print("Concatenação " + "de " + "strings.")
+
+# Aula 3 - Variaveis e constantes
+name = 'Zeca'
+age = 32
+
+name, age = 'Rafael', 34
+
+print('Aula sobre Variaveis e constantes')
+print(f"Meu nome é {name} e tenho {age} anos.")
+
+# Formas de escrever variaveis
+limiteSaqueDiario = 1000
+limite_saque_diario = 1000
+
+# Variaveis que deveriam ser conts se escrevem com todos os caracteres maiculos e Snake Case
+BRAZILEAN_STATES=["SP", "PE", "CE"]
+print(BRAZILEAN_STATES)
