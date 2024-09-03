791212ebf2a9321f2101f4d9240ab831278e506a
rafael <rafaelvrcr@gmail.com>
2024-09-03 13:48:27 -0300

    operadores de atribuição

---
+++ operadores_de_atribuicao.py
@@ -0,0 +1,27 @@
+# atribuição simples de saldo para 500
+saldo = 500
+print(saldo)
+
+# saldo aumentado por 200
+saldo += 200
+print(saldo)
+
+# saldo diminuido por 200
+saldo -= 200
+print(saldo)
+
+# saldo multiplicado por 2
+saldo *= 2
+print(saldo)
+
+# saldo dividido por 5
+saldo /= 5
+print(saldo)
+
+# saldo elevado a 5
+saldo **= 2
+print(saldo)
+
+# Saldo dividido por 5 e arredondado para cima
+saldo //= 5
+print(saldo)
