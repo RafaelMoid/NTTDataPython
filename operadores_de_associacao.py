e06f9f3d692684e31a2092478faafc7a917412d3
rafael <rafaelvrcr@gmail.com>
2024-09-03 15:02:36 -0300

    operadores de associação

---
+++ operadores_de_associacao.py
@@ -0,0 +1,7 @@
+identidade_de_cor = ["Vermelho" ,"Branco"]
+cores = ["Azul", "Preto", "Branco", "Verde", "Vermelho", "Incolor"]
+cmc = [ 3, 10 ]
+
+print(identidade_de_cor[0] and identidade_de_cor[1] in cores)
+print("Roxo" not in cores)
+print(7 in cmc)
